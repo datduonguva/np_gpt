fix softmax, crossentropy functions

src/gpt.py
@@ -34,6 +34,11 @@ class Matrix():
     def __rmul__(self, other): return self * other
     def __truediv__(self, other): return self * other ** (-1)
     def __rtruediv__(self, other): return other * self ** (-1)
+
+    def log(self):
+        return Matrix(
+            np.log(self.data + 1e-9), (self, ), (1/self.data, )
+        )
     def repeat(self, n, dim): 
         result = np.repeat(self.data, n, dim)
         return Matrix(
@@ -75,11 +80,9 @@ class Matrix():
                 v.children[0].grad += np.sum(v.grad, axis=-1, keepdims=True)
             elif v.ops == 'softmax':
                 # dL/dy.dot (s)
-                term_1 = v.grad*v.children[0].data
-                term_2 = np.sum(
-                    term_1, axis=-1, keepdims=True
-                ) * v.children[0].data
-                v.children[0].grad += term 1 - term 2
+                term_1 = v.grad*v.data
+                term_2 = np.sum(v.grad * v.data, axis=-1, keepdims=True) * v.data
+                v.children[0].grad += term_1 - term_2
             else:
                 for child, local_grad in zip(v.children, v.local_grads):
                     child.grad += v.grad * local_grad
@@ -124,8 +127,8 @@ class Softmax():
                 = v.grad * s - v.grad*s_i.sum,
     """
     def __call__(self, x: Matrix) -> Matrix:
-        max_val = np.max(y_pred.data, axis=-1, keepdims=True)
-        data = y_pred.data- max_val
+        max_val = np.max(x.data, axis=-1, keepdims=True)
+        data = x.data- max_val
         data = np.exp(data)
         data = data / np.sum(data, axis=-1, keepdims=True)
         return Matrix(
@@ -157,24 +160,16 @@ class Dropout:
 
 class CategoricalEntropy():
     def __init__(self):
-        pass
-
+        self.sum = Sum()
     def __call__(self, y_true, y_pred, training=True):
-        softmax_output = self.softmax(y_pred) 
-        batch_size = softmax_output.shape[0] 
-        # TODO: remove this softmax
-        return Matrix(
-            - np.mean((y_true.data) * np.log(softmax_output + 1e-9)), 
-            (y_pred, ), 
-            ((softmax_output - y_true.data)/batch_size, )
-        )
-    def softmax(self, y_pred: Matrix):
-        max_val = np.max(y_pred.data, axis=-1, keepdims=True)
-        data = y_pred.data- max_val
-        data = np.exp(data)
-        data = data / np.sum(data, axis=-1, keepdims=True)
-        return data
-   
+        """
+        Assuming that y_pred is already normalized by softmax and y_true is
+        1-hot encoded
+        """
+
+        batch = y_true.data.shape[0]
+        result = - self.sum(y_true * y_pred.log()) / batch
+        return result
 
 class RMSNorm2():
      def __call__(self, x: Matrix) -> Matrix:

tests/test_gpt.py
@@ -18,6 +18,7 @@ def test_1():
             self.linear2 = Linear(10, 12)
             self.linear3 = Linear(12, 4)
 
+
         def __call__(self, input_):
 
             y1 = self.linear1(input_)
@@ -196,7 +197,7 @@ def test_3():
 
 def test_4():
     """
-    Create an input, run through models with RMSNorm2
+    Test Sum layer
     """
     class MyModel:
 
@@ -205,7 +206,6 @@ def test_4():
             self.linear1 = Linear(5, 10)
             self.linear2 = Linear(10, 12)
             self.linear3 = Linear(12, 4)
-            self.norm = RMSNorm2()
             self.sum = Sum()
 
         def __call__(self, input_):
@@ -256,6 +256,65 @@ def test_4():
         assert np.abs(grad_method_1 - grad_method_2).max() < max_error
 
 
+def test_5():
+    """
+    Test softmax layer
+    """
+    class MyModel:
+
+        def __init__(self):
+
+            self.linear1 = Linear(5, 10)
+            self.linear2 = Linear(10, 4)
+            self.softmax = Softmax()
+
+        def __call__(self, input_):
+
+            y1 = self.linear1(input_)
+            y1 = self.softmax(y1)
+            y2 = self.linear2(y1)
+            y2 = self.softmax(y2)
+            return y2
+
+    input_ = Matrix(data=np.random.normal(0, 1, (3, 5)))
+
+    my_model = MyModel()
+    output_ = my_model(input_)
+
+
+    output_.backward()
+
+    EPS = 1e-6
+    # shallower layers might produce more error compared to deeper layers
+    for layer, max_error in [
+        (my_model.linear1, 1e-3),
+        (my_model.linear2, 1e-3),
+    ]:
+        grad_method_1 = layer.w.grad
+
+        # method 2:
+        grad_method_2 = np.zeros(layer.w.data.shape)
+
+        original_w = layer.w.data.copy()
+        for i in range(grad_method_2.shape[0]):
+            for j in range(grad_method_2.shape[1]):
+
+                layer.w.data = original_w.copy()
+                layer.w.data[i][j] -= EPS
+
+                y1 = my_model(input_)
+
+                
+                layer.w.data = original_w.copy()
+                layer.w.data[i][j] += EPS
+
+                y2 = my_model(input_)
+
+                grad_method_2[i][j] = (y2 - y1).data.sum() / (2*EPS)
+
+        assert np.abs(grad_method_1 - grad_method_2).max() < max_error
+
+
 def test_mnist():
     """
     This test trains an DNN model on MNIST to confirm that the loss converges.
@@ -269,13 +328,14 @@ def test_mnist():
             self.linear_3 = Linear(64 , 32)
             self.linear_4 = Linear(32, 10)
             self.relu = Relu()
+            self.softmax = Softmax()
             
         def __call__(self, input_: Matrix):
             x1 = self.relu(self.norm(self.linear_1(input_)))
             x2 = self.relu(self.norm(self.linear_2(x1)))
             x3 = self.relu(self.norm(self.linear_3(x2)))
             
-            y = self.linear_4(x3)
+            y = self.softmax(self.linear_4(x3))
             return y
 
 
@@ -305,7 +365,7 @@ def test_mnist():
         loss = loss_function(y_true, y_pred)
         if step % 20 == 0:
 
-            loss_history.append(loss.data)
+            loss_history.append(np.mean(loss.data))
             acc_history.append((np.argmax(y_true.data, axis=-1) == np.argmax(y_pred.data, axis=-1)).mean())
             print("acc: ", acc_history[-1], "loss: ", loss_history[-1])
 